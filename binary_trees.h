a22de44a347a863ce01458d49aeada75296525fd
ghalyahk <ghalyah1166@gmail.com>
2025-10-25 12:51:09 +0000

    Write a function that creates a binary tree node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,5 +21,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-#endif
+
+/* Print helper */
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif /* BINARY_TREES_H */
