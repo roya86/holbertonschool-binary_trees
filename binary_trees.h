5f95d1995ec71ade12700b4e49e034271663611c
ghalyahk <ghalyah1166@gmail.com>
2025-10-25 15:38:19 +0000

    Write a function that checks if a node is a leaf

--- binary_trees.h
+++ binary_trees.h
@@ -1,26 +1,43 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
 
-/* Function prototypes */
+/* Task 0: Create a node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1: Insert left child */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2: Insert right child */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3: Delete entire tree */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* Task 4: Check if node is a leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *);
+
+/* Helper: Print the tree */
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
-
