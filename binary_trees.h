8460be3ad54fa07f5adaa94ebc6d88d8baf18112
ghalyahk <ghalyah1166@gmail.com>
2025-10-25 17:03:53 +0000

    Add 8-binary_tree_postorder.c and 8-main.c

--- binary_trees.h
+++ binary_trees.h
@@ -6,7 +6,6 @@
 
 /**
  * struct binary_tree_s - Binary tree node
- *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -19,17 +18,12 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)); /* ← أضيفي هذا السطر */
-
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+size_t binary_tree_height(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
